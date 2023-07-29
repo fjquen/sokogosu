200a94b10048c486253a5d897b6243e8d8541e51
fjquen <fjquen@gmail.com>
2023-07-29 15:40:16 +0200

    ajout console log pour les distance

--- game/module.rb
+++ game/module.rb
@@ -38,6 +38,7 @@ module Module_move
   def right_collision_box(box_class)
       box_class.each do |box|
         puts "boxe non #{box.x} " + " x qui change #{@x_test}"
+        puts "boxe non #{box.x} " + " x + y qui change #{@x_test + box.y}"
         puts "distnace generale : #{Gosu.distance(box.x, box.y, @x_test, @y_test).round}"
         puts "distance sous condition #{Gosu.distance(box.x, box.y, @x_test, @y_test).round==@x_test}"
       end
