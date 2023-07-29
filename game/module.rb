1e1975a1ec7b2a2a3dc35605325760bc49c53391
fjquen <fjquen@gmail.com>
2023-07-29 18:21:33 +0200

    test collision

--- game/module.rb
+++ game/module.rb
@@ -39,10 +39,6 @@ module Module_move
       box_class.each do |box|
         puts "boxe non changer #{box.x} " + " x qui change #{@x_test}"
         puts "distance generale : #{Gosu.distance(box.x, box.y, @x_test, @y_test).round}"
-        puts @y
-        if box.x <= @x_test and box.y >= @y_test
-          puts 'bingo'
-        end
       end
   end
 
