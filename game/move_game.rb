ea8826c84bfc4e753338c8a7d2bb909b2b58d453
fjquen <fjquen@gmail.com>
2023-08-09 17:05:15 +0200

    mise en place de la boucle each pour la collision

--- game/move_game.rb
+++ game/move_game.rb
@@ -44,21 +44,14 @@ module Move_game
     end
 
     def push_box(player,box)
-        pos_row_player = @area1.flatten.index(player)
-        pos_col_player = @area1.first.size
-        row_player = pos_row_player / pos_col_player
-        col_player = pos_row_player % pos_col_player
-        @x = col_player
-        @x += 1
-
-        pos_row_box = @area1.flatten.index(box)
-        pos_col_box = @area1.first.size
-        row_box = pos_row_box / pos_col_box
-        col_box = pos_row_box % pos_col_box
-        @x_push = col_box
-        @x_push += 1
-        return if @area1[row_player][@x] == @area1[row_box][@x_push]
-            @area1[row_box][col_box],@area1[row_box][@x_push] = @area1[row_box][@x_push],@area1[row_box][col_box]
-            @area1[row_player][@x],@area1[row_box][@x_push] = @area1[row_box][@x_push],@area1[row_player][@x]
+        @area1.each_index do |y|
+            @area1[y].each_index do |x|
+                if @area1[y][x] == player
+                    if @area1[y][x] == box
+                        puts "u"
+                    end
+                end
+            end
+        end
     end
 end
