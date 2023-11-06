22d6f88de63aa96aa832af9fbb0b6651eb485a5a
fjquen <fjquen@gmail.com>
2023-11-06 14:27:16 +0100

    factorisation module Move_game

--- game/move_game.rb
+++ game/move_game.rb
@@ -14,14 +14,17 @@ module Move_game
             when obj_move["l"]
                 @x = col_player
                 @x -= 1
+                check_collision(row_player,col_player,wall,block,goal,void,win,obj_move,dir_move)
                 return_move_wall(axe["x"],row_player,col_player,wall,axe)
             when obj_move["u"]
                 @y = row_player
                 @y -= 1
+                check_collision(row_player,col_player,wall,block,goal,void,win,obj_move,dir_move)
                 return_move_wall(axe["y"],row_player,col_player,wall,axe)
             when obj_move["d"]
                 @y = row_player
                 @y += 1
+                check_collision(row_player,col_player,wall,block,goal,void,win,obj_move,dir_move)
                 return_move_wall(axe["y"],row_player,col_player,wall,axe)
             else
                 puts "mauvaise valeur objet #{dir_move}"
@@ -47,129 +50,51 @@ module Move_game
             @area1[y].each_index do |x|
                 if @area1[y][x] == block
                     if @area1[row][@x] == block
-                        if row==y && x==@x
-                            @x_push = x
-                            @x_push += 1
-                            if @area1[y][@x_push] == block && @area1[y][x] == block
-                                @x = col
+                            if row==y && x==@x
+                               @x_push = x
+                               case dir_move
+                                    when obj_move["r"]
+                                        @x_push += 1
+                                    when obj_move["l"]
+                                        @x_push -= 1
+                                    else
+                                        puts "Valeur inconnu"
+                               end
+                                
+                                if @area1[y][@x_push] == block && @area1[y][x] == block
+                                    @x = col
+                                end
+                                if @area1[y][@x_push] == goal
+                                    @area1[y][@x_push] = void
+                                    @area1[y][x] = win
+                                    @point_win += 1
+                                end
+                                if @area1[y][@x_push] == win && @area1[y][x] == block
+                                    case dir_move
+                                        when obj_move["r"]
+                                            @x_push = x-1
+                                        when obj_move["l"]
+                                            @x_push = x+1
+                                        else
+                                            puts "Valeur inconnu"
+                                    end
+                                end
+                                return if @area1[y][@x_push] == wall
+                                        @area1[y][@x_push],@area1[y][x] = @area1[y][x],@area1[y][@x_push]
                             end
-                            if @area1[y][@x_push] == goal
-                                @area1[y][@x_push] = void
-                                @area1[y][x] = win
-                                @point_win += 1
-                            end
-                            if @area1[y][@x_push] == win && @area1[y][x] == block
-                                @x_push = x-1
-                            end
-                            return if @area1[y][@x_push] == wall
-                            @area1[y][@x_push],@area1[y][x] = @area1[y][x],@area1[y][@x_push]
-                        end
-                    end
-                end
-            end
-        end
-    end
-
-    def right_move_push(player,wall,block,goal,void,win)
-        pos_row_player = @area1.flatten.index(player)
-        pos_col_player = @area1.first.size
-        row_player = pos_row_player / pos_col_player
-        col_player = pos_row_player % pos_col_player
-        @x = col_player
-        @x += 1
-        @area1.each_index do |y|
-            @area1[y].each_index do |x|
-                if @area1[y][x] == block
-                    if @area1[row_player][@x] == block
-                        if row_player==y && x==@x
-                            @x_push = x
-                            @x_push += 1
-                            if @area1[y][@x_push] == block && @area1[y][x] == block
-                                @x = col_player
-                            end
-                            if @area1[y][@x_push] == goal
-                                @area1[y][@x_push] = void
-                                @area1[y][x] = win
-                                @point_win += 1
-                            end
-                            if @area1[y][@x_push] == win && @area1[y][x] == block
-                                @x_push = x-1
-                            end
-                            return if @area1[y][@x_push] == wall
-                            @area1[y][@x_push],@area1[y][x] = @area1[y][x],@area1[y][@x_push]
-                        end
-                    end
-                end
-            end
-        end
-        if @area1[row_player][@x] == win
-            @x = col_player
-        end
-        if @area1[row_player][@x] == goal
-            @x = col_player
-        end
-        return if @area1[row_player][@x] == wall
-            @area1[row_player][col_player],@area1[row_player][@x] = @area1[row_player][@x],@area1[row_player][col_player]
-    end 
-
-    def left_move_push(player,wall,block,goal,void,win)
-        pos_row_player = @area1.flatten.index(player)
-        pos_col_player = @area1.first.size
-        row_player = pos_row_player / pos_col_player
-        col_player = pos_row_player % pos_col_player
-        @x = col_player
-        @x -= 1
-        @area1.each_index do |y|
-            @area1[y].each_index do |x|
-                if @area1[y][x] == block
-                    if @area1[row_player][@x] == block
-                        if row_player==y && x==@x
-                            @x_push = x
-                            @x_push -= 1
-                            if @area1[y][@x_push] == block && @area1[y][x] == block
-                                @x = col_player
-                            end
-                            if @area1[y][@x_push] == goal
-                                @area1[y][@x_push] = void
-                                @area1[y][x] = win
-                                @point_win += 1
-                            end
-                            if @area1[y][@x_push] == win && @area1[y][x] == block
-                                @x_push = x+1
-                            end
-                            return if @area1[y][@x_push] == wall
-                            @area1[y][@x_push],@area1[y][x] = @area1[y][x],@area1[y][@x_push]
-                        end
-                    end
-                end
-            end
-        end
-        if @area1[row_player][@x] == win
-            @x = col_player
-        end
-        if @area1[row_player][@x] == goal
-            @x = col_player
-        end
-        return if @area1[row_player][@x] == wall
-            @area1[row_player][col_player],@area1[row_player][@x] = @area1[row_player][@x],@area1[row_player][col_player]
-    end
-    
-    def up_move_push(player,wall,block,goal,void,win)
-        pos_row_player = @area1.flatten.index(player)
-        pos_col_player = @area1.first.size
-        row_player = pos_row_player / pos_col_player
-        col_player = pos_row_player % pos_col_player
-        @y = row_player
-        @y -= 1
-        @area1.each_index do |y|
-            @area1[y].each_index do |x|
-                if @area1[y][x] == block
-                    if @area1[@y][col_player] == block
-                        if col_player==x && y==@y
+                    elsif @area1[@y][col] == block
+                        if col ==x && y==@y
                             @y_push = y
-                            @y_push -= 1
+                            case dir_move
+                                when obj_move["u"]
+                                    @y_push -= 1
+                                when obj_move["d"]
+                                    @y_push += 1
+                                else
+                                    puts "Valeur inconnu"
+                            end
                             if @area1[@y_push][x] == block && @area1[y][x] == block
-                                @y = row_player
+                                @y = row
                             end
                             if @area1[@y_push][x] == goal
                                 @area1[@y_push][x] = void
@@ -177,64 +102,39 @@ module Move_game
                                 @point_win += 1
                             end
                             if @area1[@y_push][x] == win && @area1[y][x] == block
-                                @y_push = y+1
+                                case dir_move
+                                    when obj_move["u"]
+                                        @y_push = y-1
+                                    when obj_move["d"]
+                                        @y_push = y+1
+                                    else
+                                        puts "Valeur inconnu"
+                                end
                             end
                             return if @area1[@y_push][x] == wall
-                            @area1[@y_push][x],@area1[y][x] = @area1[y][x],@area1[@y_push][x]
+                                    @area1[@y_push][x],@area1[y][x] = @area1[y][x],@area1[@y_push][x]
                         end
                     end
                 end
             end
         end
-        if @area1[@y][col_player] == win
-            @y = row_player
-        end
-        if @area1[@y][col_player] == goal
-            @y = row_player
-        end
-        return if @area1[@y][col_player] == wall
-            @area1[row_player][col_player],@area1[@y][col_player] = @area1[@y][col_player],@area1[row_player][col_player]
-    end
-
-    def down_move_push(player,wall,block,goal,void,win)
-        pos_row_player = @area1.flatten.index(player)
-        pos_col_player = @area1.first.size
-        row_player = pos_row_player / pos_col_player
-        col_player = pos_row_player % pos_col_player
-        @y = row_player
-        @y += 1
-        @area1.each_index do |y|
-            @area1[y].each_index do |x|
-                if @area1[y][x] == block
-                    if @area1[@y][col_player] == block
-                        if col_player==x && y==@y
-                            @y_push = y
-                            @y_push += 1
-                            if @area1[@y_push][x] == block && @area1[y][x] == block
-                                @y = row_player
-                            end
-                            if @area1[@y_push][x] == goal
-                                @area1[@y_push][x] = void
-                                @area1[y][x] = win
-                                @point_win += 1
-                            end
-                            if @area1[@y_push][x] == win && @area1[y][x] == block
-                                @y_push = y-1
-                            end
-                            return if @area1[@y_push][x] == wall
-                            @area1[@y_push][x],@area1[y][x] = @area1[y][x],@area1[@y_push][x]
-                        end
+            case dir_move
+                when obj_move["r"], obj_move["l"]
+                    if @area1[row][@x] == win
+                        @x = col
                     end
-                end
-            end
-        end
-        if @area1[@y][col_player] == win
-            @y = row_player
-        end
-        if @area1[@y][col_player] == goal
-            @y = row_player
-        end
-        return if @area1[@y][col_player] == wall
-            @area1[row_player][col_player],@area1[@y][col_player] = @area1[@y][col_player],@area1[row_player][col_player]
+                    if @area1[row][@x] == goal
+                        @x = col
+                    end
+                when obj_move["u"], obj_move["d"]
+                    if @area1[@y][col] == win
+                        @y = row
+                    end
+                    if @area1[@y][col] == goal
+                        @y = row
+                    end
+                else
+                    puts "Valeur inconnu"
+            end         
     end
 end
